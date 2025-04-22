c062a2797c197807fe02a75607a71e80165fea54
alhatsaurabh <alhatsaurabh@gmail.com>
2025-04-22 10:43:48 +0000

    Fix(glob): Include public dir in relative paths for markdown files

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -88,8 +88,21 @@ let caseStudyModules: Record<string, MarkdownModule> = {};
 
 function initializeModules() {
   try {
-    blogPostModules = import.meta.glob<MarkdownModule>('./src/posts/blog/*.md', { eager: true });
-    caseStudyModules = import.meta.glob<MarkdownModule>('./src/posts/case-studies/*.md', { eager: true });
+    blogPostModules = import.meta.glob<MarkdownModule>([
+      './src/posts/blog/*.md',
+      './public/src/posts/blog/*.md'
+    ], { eager: true });
+    
+    caseStudyModules = import.meta.glob<MarkdownModule>([
+      './src/posts/case-studies/*.md',
+      './public/src/posts/case-studies/*.md'
+    ], { eager: true });
+    
+    // Add debug logging
+    console.log('Blog post modules available:', Object.keys(blogPostModules).length);
+    console.log('Case study modules available:', Object.keys(caseStudyModules).length);
+    console.log('Blog post modules paths:', Object.keys(blogPostModules));
+    console.log('Case study modules paths:', Object.keys(caseStudyModules));
     
     // Add debug logging
     console.log('Blog post modules available:', Object.keys(blogPostModules).length);
